122994c0bc841eac49ab7ae4be82c3543d2b28f1
jungyoon <kim2005gd@gmail.com>
2026-01-20 14:00:12 +0900

    Title: 길이 단위 변환하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260120/길이 단위 변환하기/change-length-unit.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    float a=9.2;
+    float b=1.3;
+    printf("%.1lfft = %.1lfcm\n%.1lfmi = %.1lfcm",a,a*30.48,b,b*160934);
+    return 0;
+}
