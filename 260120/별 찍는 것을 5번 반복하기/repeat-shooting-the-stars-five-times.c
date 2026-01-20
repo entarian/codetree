473bb1961ba19661d3a3cc05b88a38e549a3b657
jungyoon <kim2005gd@gmail.com>
2026-01-20 14:29:43 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260120/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void Print10Stars(){
+    for(int i=0;i<10;i++){
+        printf("*");
+    
+    }
+    printf("\n");
+}
+int main() {
+    for(int i=0;i<5;i++){
+        Print10Stars();
+    }
+    // Please write your code here.
+    return 0;
+}
