bb73622d99bcf08d4b721f146577031919593351
jungyoon <kim2005gd@gmail.com>
2026-01-20 18:01:22 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260120/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void prime(int a, int b){
+    int pri=1;
+    int total=0;
+    for(int i=a;i<=b;i++){
+        for(int r=2;r<i;r++){
+            if(i%r==0){
+                pri=0;
+                break;
+            }
+            else{
+                int c,d;
+                c=i/10;
+                d=i%10;
+                if((c+d)%2==0){
+                    pri=1;
+                }
+            }
+        }
+        if(pri==1){
+            total++;
+        }
+    }
+    printf("%d",total);
+}
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    prime(a,b);
+    // Please write your code here.
+    
+    return 0;
+}
