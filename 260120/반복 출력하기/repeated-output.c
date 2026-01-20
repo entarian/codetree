0e4ff92ffc181252b08d7c68d82e3bc9e1532d39
jungyoon <kim2005gd@gmail.com>
2026-01-20 14:33:10 +0900

    Title: 반복 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260120/반복 출력하기/repeated-output.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void Prints(int n){
+    for(int i=0;i<n;i++){
+        printf("12345^&*()_");
+        printf("\n");
+    }
+    
+}
+
+int main() {
+    int row_num;
+    scanf("%d", &row_num);
+    Prints(row_num);
+    // Please write your code here.
+    return 0;
+}
