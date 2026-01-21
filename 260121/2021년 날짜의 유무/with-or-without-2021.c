42010e79e85e972332c54bbba8c65428af49c09e
jungyoon <kim2005gd@gmail.com>
2026-01-21 09:40:48 +0900

    Title: 2021년 날짜의 유무, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260121/2021년 날짜의 유무/with-or-without-2021.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+void cal(int m,int d){
+    int match=1;
+    for(int i=1;i<=12;i++){
+        int n=0;
+        match=1;
+        if(m==1||m==3||m==5||m==7||m==8||m==10||m==12){
+            n=31;
+        }
+        else if(m==4||m==6||m==9||m==11){
+            n=30;
+        }
+        else if(m==2){
+            n=28;
+        }
+        else{
+            printf("No");
+        }
+        for(int r=1;r<=n;r++){
+                if(r!=d){
+                    match=0;
+                    continue;
+                    
+                }
+                else{
+                    match=1;
+                    break;
+                }
+            }
+    }
+    if(match==1){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+}
+int main() {
+    int m, d;
+    scanf("%d %d", &m, &d);
+    cal(m,d);
+    // Please write your code here.
+    return 0;
+}
