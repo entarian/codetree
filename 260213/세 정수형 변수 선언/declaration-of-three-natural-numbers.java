02fafeced7f85acf50732572408385ba3d36daae
jungyoon <kim2005gd@gmail.com>
2026-02-13 16:18:11 +0900

    Title: 세 정수형 변수 선언, Time: 107ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260213/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.\
+        int a,b,c;
+        a=7;
+        b=23;
+        c=30;
+        System.out.printf("%d + %d = %d",a,b,c);
+    }
+}
